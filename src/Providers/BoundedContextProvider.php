eb9b50491bfb71bbcbaea90aedf92e69fdc9c9bb
Colin Lyons <colin@tercet.io>
2015-08-24 14:29:01 +0100

    Adding Console Commands and Provider.

---
+++ src/Providers/BoundedContextProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace BoundedContext\Laravel\Providers;
+
+use BoundedContext\Laravel\Log\InMemoryLog;
+use BoundedContext\Map\Map;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+
+class BoundedContextProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->bind(
+            'Illuminate\Contracts\Bus\Dispatcher',
+            'BoundedContext\Laravel\Bus\Dispatcher'
+        );
+
+        $projections = Config::get('projections');
+        foreach($projections as $projection => $implemented_projection)
+        {
+            $this->app->bind($projection, $implemented_projection);
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
